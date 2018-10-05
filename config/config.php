3673167f98c43d8025c13d599489914830df522d
LittleJake <465917717@qq.com>
2018-10-05 15:34:18 +0800

    分离config文件

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jake
+ * Date: 10/5/2018
+ * Time: 2:33 PM
+ */
+
+define('APP', EMP.'app/');
+define('CORE', EMP.'emp/');
+define('DEBUG', true);
+
