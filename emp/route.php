a637ad396f75c95bc3dd3995983c9f5b607549d8
LittleJake <465917717@qq.com>
2018-10-05 15:35:57 +0800

    增加路由类

---
+++ emp/route.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jake
+ * Date: 10/5/2018
+ * Time: 2:49 PM
+ */
+
+namespace emp;
+
+
+class route
+{
+    public function __construct(){
+        p('route ok');
+    }
+
+}
