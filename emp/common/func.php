10f8559fa808647dbeeb68955a987a62862fec51
LittleJake <465917717@qq.com>
2018-10-07 17:20:39 +0800

    修改p函数

--- emp/common/func.php
+++ emp/common/func.php
@@ -6,6 +6,11 @@
  * Time: 2:30 PM
  */
 
-function p($str){
-    echo $str.'<br>';
+function p($s){
+    if(is_bool($s))
+        var_dump($s);
+    else if(is_null($s))
+        var_dump('NULL');
+    else
+        echo "<pre style=\"border: 1px solid;border-radius: 5px;position: relative;z-index: 1000;padding: 10px;font-size: 18px;line-height: 23px;background: lightskyblue; opacity: 0.9; \">". print_r($s, true) .'</pre>';
 }
