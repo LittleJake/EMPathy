abeb909cf193c2e622ff87f3d3ee4a182ce09b32
LittleJake <465917717@qq.com>
2018-10-07 17:19:29 +0800

    添加数据库配置文件

---
+++ config/db_config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jake
+ * Date: 10/5/2018
+ * Time: 7:46 PM
+ */
+
+return [
+    'name' => 'emp',
+    'user' => 'root',
+    'pass' => '',
+    'addr' => '127.0.0.1',
+    'port' => '3306',
+];
