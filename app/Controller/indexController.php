2e92e7d90790f2e7292712c662059de0242485a0
LittleJake <465917717@qq.com>
2019-01-30 18:22:37 +0800

    update .gitignore

---
+++ app/Controller/indexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jake
+ * Date: 10/8/2018
+ * Time: 5:32 PM
+ */
+
+namespace app\Controller;
+
+
+class indexController
+{
+    public function indexAction()
+    {
+        p('index controller index action');
+    }
+}
